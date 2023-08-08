2e04e1338055ac56fa239887c46654a269bb6773
UsboKirishima <usbertibox@gmail.com>
2023-08-08 20:15:47 +0200

    feat(ui): Login screen

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SRC := $(foreach x, ./src, $(wildcard $(addprefix $(x)/*,.c*)))
 DIRS := $(foreach x, ./src/**, $(wildcard $(addprefix $(x)/*,.c*)))
-INCLUDES:=$(shell pkg-config --cflags libavformat libavcodec libswresample libswscale libavutil gtk4)
-LIBS=$(shell pkg-config --libs gtk4 )
+INCLUDES:=$(shell pkg-config --cflags libavformat libavcodec libswresample libswscale libavutil gtk+-3.0)
+LIBS=$(shell pkg-config --libs gtk+-3.0 )
 out.o: 
 	gcc ${SRC} -I include ${INCLUDES} ${DIRS} -o build/krokodil -pthread -ldiscord -lcurl ${LIBS} && ./build/krokodil
--- src/kk_ui.c
+++ src/kk_ui.c
@@ -1,49 +1,56 @@
 #include <kk_ui.h>
 
-void
-print_hello(GtkWidget *widget,
-            gpointer data)
+void login_button_pressed(GtkWidget *widget,
+                          gpointer data)
 {
-    g_print("Hello World");
+    g_print(gtk_entry_get_text(data));
 }
 
-void
-activate(GtkApplication *app,
-         gpointer user_data)
+void activate(GtkApplication *app,
+              gpointer user_data)
 {
     GtkWidget *window;
     GtkWidget *button;
     GtkWidget *box;
+    GtkWidget *key_box;
+    GtkWidget *key_label;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
-    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    box = gtk_button_box_new(GTK_ORIENTATION_VERTICAL);
     gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
+    gtk_container_add(GTK_CONTAINER(window), box);
 
-    gtk_window_set_child(GTK_WINDOW(window), box);
+    key_label = gtk_label_new("Enter your key:");
+    gtk_container_add(GTK_CONTAINER(box), key_label);
+    gtk_widget_set_halign(key_label, GTK_ALIGN_START);
 
-    button = gtk_button_new_with_label("Hello World");
+    key_box = gtk_entry_new();
+    gtk_entry_set_max_length(GTK_ENTRY(key_box), 0);
+    gtk_container_add(GTK_CONTAINER(box), key_box);
 
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_window_destroy), window);
+    button = gtk_button_new_with_label("Login");
+    gtk_container_add(GTK_CONTAINER(box), button);
+    g_signal_connect(button, "clicked", G_CALLBACK(login_button_pressed), key_box);
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
 
-    gtk_box_append(GTK_BOX(box), button);
+    // Margins
+    g_object_set(key_box, "margin-bottom", 20, NULL);
+    g_object_set(key_label, "margin-bottom", 5, NULL);
 
-    gtk_widget_show(window);
+    gtk_widget_show_all(window);
 }
 
-void
-*startUI()
+void *startUI()
 {
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new("org.gtk.krokodil", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("org.gtk.krokodil", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), NULL, NULL);
     g_object_unref(app);
-
 }
